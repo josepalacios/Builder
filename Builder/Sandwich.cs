62864597fa0b89270888b1cf44d236e9d6c5be70
josepalacios <jose.palaciosf@gmail.com>
2019-11-24 13:50:41 -0500

    first commit

---
+++ Builder/Sandwich.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Sandwich
+    {
+        public readonly Breadtype breadtype;
+        public bool isToasted;
+        public readonly List<string> vegetables;
+    }
+}
